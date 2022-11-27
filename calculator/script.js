b848f57280af7a3347a37f8d50f3f97116c6ee1a
shubhamdarwante2301 <shubhamdarwante2301@gmail.com>
2022-11-27 16:40:07 +0530

    multiple calculation simultaniously

--- calculator/script.js
+++ calculator/script.js
@@ -1,59 +1,84 @@
 "use strict";
 
-var buttons = document.querySelectorAll('.button');
-var output = document.getElementById('output');
-// console.log(input);
+var buttons = document.querySelectorAll(".button");
+var output = document.getElementById("output");
+var expression = document.getElementById("expression");
+// console.log(expression);
 let operand1 = "";
 let operand2 = "";
 let operator = "";
-let sign = "";
-let res =  "";
-function calculation() {
-    let value = this.getAttribute('data-value');
-    // console.log(value);
-    if(value === "AC") {
-        operand1 = "";
-        operand2 = "";
-        operator = "";
-        sign = "";
-        output.value = "";
-    } else if(value=="+" || value=="-" || value=="*" || value=="/" || value=="%" || value=="pow"){
-        operator = value;
-        output.value = "";
-        
-    } else if(value=="=") {
-        let x = parseFloat(operand1);
-        let y = parseFloat(operand2);
+let res = "";
+function calculate(value) {
+  let x = parseFloat(operand1);
+  let y = parseFloat(operand2);
 
-        if(operator === "+") {
-            res = x + y;
-        } else if(operator === "-") {
-            res = x - y;
-        } else if(operator === "/") {
-            res = x / y;
-        } else if(operator === "*") {
-            res = x * y;
-        } else if(operator === "%") {
-            res = (x/100) * y;
-        } else if(operator === "pow") {
-            res = Math.pow(operand1, operand2);
-        }
-        operand1 = "";
-        operand2 = "";
-        operator = "";
-        sign = "";
-        output.value = res;
-    } else {
-        if(operator=="") {
-            operand1 += value;
-            output.value = operand1;
-        } else {
-            operand2 += value;
-            output.value = operand2;
-        }
-    }
-    // console.log(operand1, operand2, operator);
+  if(operand1==="" || operand2==="" || operator==="") {
+    output.value = res;
+    clearCalci();
+    return;
+  }
+
+  if (operator === "+") {
+    res = x + y;
+  } else if (operator === "-") {
+    res = x - y;
+  } else if (operator === "/") {
+    res = x / y;
+  } else if (operator === "*") {
+    res = x * y;
+  } else if (operator === "%") {
+    res = (x / 100) * y;
+  } else if (operator === "pow") {
+    res = Math.pow(operand1, operand2);
+  }
+
+  if (value === "=") {
+    operand1 = res;
+    operand2 = "";
+    operator = "";
+    output.value = res;
+    expression.innerText = "";
+  } else {
+    operand1 = res;
+    operand2 = "";
+    operator = "";
+    // console.log(operand1 + operator + operand2);
+    expression.innerText = res;
+  }
 }
-for(var i=0; i<buttons.length; i++) {
-    buttons[i].addEventListener('click', calculation);
+
+function clearCalci() {
+  operand1 = "";
+  operand2 = "";
+  operator = "";
+  output.value = "";
+  expression.innerText = "";
+}
+
+function calculation() {
+  let value = this.getAttribute("data-value");
+  output.value = output.value + value;
+  // console.log(value);
+  if (value === "AC") {
+    clearCalci();
+    return;
+  } else if (value == "+" || value == "-" || value == "*" || value == "/" || value == "%" || value == "pow") {
+    if (operand1 != "" && operator != "" && operand2 != "") {
+      calculate(value);
+    }
+    operator = value;
+    return;
+  } else if (value == "=") {
+    calculate(value);
+    return;
+  } else {
+    if (operator == "") {
+      operand1 += value;
+    } else {
+      operand2 += value;
+    }
+  }
+}
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", calculation);
 }
